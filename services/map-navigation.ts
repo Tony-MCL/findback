234cc5ac8376fb4c991cd1744cdcf13b4f074e61
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-07-30 11:43:32 +0200

    Open saved location as map marker

--- services/map-navigation.ts
+++ services/map-navigation.ts
@@ -6,11 +6,12 @@ export async function openSavedLocationInMaps(
   location: SavedLocation,
 ): Promise<void> {
   const coordinates = `${location.latitude},${location.longitude}`;
+  const encodedLabel = encodeURIComponent('FindBack');
 
   const url = Platform.select({
-    ios: `http://maps.apple.com/?daddr=${coordinates}&dirflg=d`,
-    android: `google.navigation:q=${coordinates}&mode=d`,
-    default: `https://www.google.com/maps/dir/?api=1&destination=${coordinates}`,
+    ios: `http://maps.apple.com/?ll=${coordinates}&q=${encodedLabel}`,
+    android: `https://www.google.com/maps/search/?api=1&query=${coordinates}`,
+    default: `https://www.google.com/maps/search/?api=1&query=${coordinates}`,
   });
 
   if (!url) {
